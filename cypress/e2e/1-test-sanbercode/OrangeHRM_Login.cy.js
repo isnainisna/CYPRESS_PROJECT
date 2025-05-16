d5577b26ba13216446e8ffcfaed0430e1d7830ad
isna <isnainis2003@gmail.com>
2025-05-16 22:38:21 +0700

    https://github.com/isnainisna/cypress-automation-tests.git

--- cypress/e2e/1-test-sanbercode/OrangeHRM_Login.cy.js
+++ cypress/e2e/1-test-sanbercode/OrangeHRM_Login.cy.js
@@ -1,59 +1,50 @@
 //Login Valid
 describe('Login OrangeHRM', ()=> {
+    beforeEach(() => {
+    
+      cy.visit('https://opensource-demo.orangehrmlive.com')
+    });
+
     it ('TC1 loginValid',()=> {
-        cy.visit('https://opensource-demo.orangehrmlive.com/')
         cy.get('input[name="username"]').type('Admin');
         cy.xpath("//input[@name='password']").clear().type('admin123')
         cy.get('.oxd-button').click()   
-    });
-});
+    })
 
 //Login_Invalid_username
-describe('Login OrangeHRM', ()=> {
     it ('TC2 loginValid username',()=> {
-        cy.visit('https://opensource-demo.orangehrmlive.com/')
         cy.get('input[name="username"]').type('adminn');
         cy.xpath("//input[@name='password']").clear().type('admin123')
         cy.get('.oxd-button').click()   
-    });
-});
+    })
 
 //Login_invalid_password
-describe('Login OrangeHRM', ()=> {
     it ('TC3 login inValid password',()=> {
-        cy.visit('https://opensource-demo.orangehrmlive.com/')
         cy.get('input[name="username"]').type('admin');
         cy.xpath("//input[@name='password']").clear().type('admin1234')
         cy.get('.oxd-button').click()   
-    });
-});
+    })
 
 //Login_invalid_password dan username
-describe('Login invalid password dan username salah', ()=> {
-    it ('TC4 loginValid',()=> {
-        cy.visit('https://opensource-demo.orangehrmlive.com/')
+
+    it ('TC4 login inValid',()=> {
         cy.get('input[name="username"]').type('adminn');
         cy.xpath("//input[@name='password']").clear().type('admin1234')
         cy.get('.oxd-button').click()   
-    });
-});
+    })
+
 
 //Login_invalid_password dan username tidak diisi
-describe('Login OrangeHRM', ()=> {
     it ('TC5 login inValid username dan password tidak diisi',()=> {
-        cy.visit('https://opensource-demo.orangehrmlive.com/')
         cy.get('input[name="username"]').type('');
         cy.xpath("//input[@name='password']").clear().type('')
         cy.get('.oxd-button').click()   
-    });
-});
+    })
 
 //Login_invalid_password dan username menggunakan spesial karakter
-describe('TC6 Login Invalid OrangeHRM', ()=> {
-    it ('TC4 login inValid username dan password tidak diisi',()=> {
-        cy.visit('https://opensource-demo.orangehrmlive.com/')
+    it ('TC6 login inValid username dan password Menggunkaan spesial karakter',()=> {
         cy.get('input[name="username"]').type(',');
         cy.xpath("//input[@name='password']").clear().type('admin12!')
         cy.get('.oxd-button').click()   
-    });
+    })
 });
